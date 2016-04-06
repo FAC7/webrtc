0523eb5e2aa7b77a55f1edc751447e1f3d4c50f8
Andrew <a.macmurray@icloud.com>
2016-04-06 17:54:40 +0100

    remove component from home

--- frontend/src/js/components/Home/index.js
+++ frontend/src/js/components/Home/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Jumbotron } from 'react-bootstrap'
-import ProfileForm from '../MentorSignup/EditProfileForm.js'
 
 export default (props) => {
   return (
-    <ProfileForm />
+    <Jumbotron className='home'>
+      <h1>Hello Home!</h1>
+    </Jumbotron>
   )
 }

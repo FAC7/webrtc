43023c37c78142553910a9f6eeb1c4c867ba35c8
spudtrader <jmurphy.web@gmail.com>
2016-04-07 15:11:49 +0100

    initialise pbx in room componetnt

--- frontend/src/js/components/Room/MentorList.jsx
+++ frontend/src/js/components/Room/MentorList.jsx
@@ -2,17 +2,7 @@ import React from 'react'
 
 class MentorList extends React.Component {
   componentDidMount () {
-    //ajax call for all mentors from our API
-    var getMentors = new XMLHttpRequest()
-    getMentors.onLoad = function (e) {
-      if (getMentors.readyState === 4 && getMentors.status === 200) {
-        console.log(getMentors.responseText)
-      } else {
-        console.log(getMentors.statusText)
-      }
-    }
-    getMentors.open('GET', 'getMentors')
-    getMentors.send()
+
   }
 
   render () {

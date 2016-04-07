673544fe78be13eb8ab629a4610c5a7e0991737a
spudtrader <jmurphy.web@gmail.com>
2016-04-07 16:36:46 +0100

    try fixing issues with App.js file

--- frontend/src/js/views/App.js
+++ frontend/src/js/views/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Videochat from './Videochat/index.js'
 import Header from '../components/Header/index.js'
 import Footer from '../components/Footer/index.js'
 
@@ -15,7 +14,6 @@ export default class App extends React.Component {
     return (
       <div>
         <Header menuItems={options.menuItems} logoUrl={options.logoUrl} fluid />
-        <Videochat />
         <div id="contacts">
     		  <div class="contacts-title">Contacts</div>
     	  </div>

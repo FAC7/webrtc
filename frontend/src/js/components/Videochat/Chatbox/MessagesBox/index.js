b7252569e97bc826d6965abf00dceb88020299e4
franzmoro <franzmoro88@gmail.com>
2016-04-08 15:37:33 +0100

    fixed two elements with same id bug(!!)

--- frontend/src/js/components/Videochat/Chatbox/MessagesBox/index.js
+++ frontend/src/js/components/Videochat/Chatbox/MessagesBox/index.js
@@ -5,7 +5,7 @@ export default (props) => {
   return (
     <Row className='show-grid' id='messages'>
       <Col xs={18} md={12} className='flush'>
-        <div id='textbox' style={props.messagesBoxStyle}></div>
+        <div id='messages' style={props.messagesBoxStyle}></div>
       </Col>
     </Row>
   )

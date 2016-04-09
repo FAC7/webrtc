a0f78819e97d59f53194c5c8c1de7946cba944a0
Elias <elias0789@gmail.com>
2016-04-09 10:52:23 +0100

    add twitter route back

--- backend/lib/routes/TwitterLogin.js
+++ backend/lib/routes/TwitterLogin.js
@@ -1,8 +1,54 @@
+import jwt from 'jsonwebtoken'
+import getUserData from '../redis/redisFunctions.js'
+
 export default {
   method: ['GET', 'POST'],
   path: '/auth/{mode}/{usertype}',
   config: {
     auth: 'twitter',
-    handler: () => {}
+    handler: (request, reply) => {
+      const mode = request.params.mode
+      const type = request.params.usertype
+      if (request.auth.isAuthenticated) {
+        const cred = request.auth.credentials
+        const dataToSend = {
+          token: cred.token,
+          secret: cred.secret,
+          screenName: cred.profile.raw.screen_name
+        }
+        const jwToken = jwt.sign(dataToSend, process.env.JWT_SECRET)
+        request.cookieAuth.set({twitterCookie: jwToken})
+        // request.cookieAuth.set({test: 'ivan'})
+
+
+        if (mode === 'login') {
+          if (type === 'mentor') {
+            getUserData('mentors', dataToSend.screenName, (data) => {
+              reply(data)
+            })
+          } else {
+            getUserData('mentees', dataToSend.screenName, (data) => {
+              reply(data)
+            })
+          }
+        }
+
+        if (mode === 'signup') {
+          if (type === 'mentor') {
+            console.log('signed up as mentor')
+            var response = JSON.stringify(reply)
+            reply(response)
+            // reply.redirect('/')
+          } else {
+            console.log('signed up as mentee')
+            reply({
+              isLoginSuccessful: true,
+              isSignedUp: false,
+              isMentor: false
+            })
+          }
+        }
+      }
+    }
   }
 }

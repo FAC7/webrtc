a1257c7c0266ec5f9f8aa22e3aa00483a9e8097a
Elias <elias0789@gmail.com>
2016-04-09 17:04:39 +0100

    Add ipc user and respond with IPC deets

--- backend/lib/routes/api/profile/setUserProfile.js
+++ backend/lib/routes/api/profile/setUserProfile.js
@@ -1,11 +1,17 @@
 import {setUserProfile} from '../../../redis/redisFunctions.js'
+import ipcAddUser from '../../../redis/ipcAddUser.js'
 
 export default (client, userType, username, payload, reply) => {
-  setUserProfile(client, userType, username, payload)
-  .then((result) => {
-    reply({success: true, data: result})
-  })
-  .catch((err) => {
-    reply({success: false, data: err})
-  })
+  ipcAddUser(username, payload.firstName + payload.lastName)
+    .then((result) => {
+      payload.apiId = result.newUserData.uname
+      payload.apiPassword = result.newUserData.password
+      return setUserProfile(client, userType, username, payload)
+    })
+    .then(() => {
+      reply({success: true, data: payload})
+    })
+    .catch((err) => {
+      reply({success: false, data: err})
+    })
 }

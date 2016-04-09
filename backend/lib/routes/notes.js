7e37a0c2da07840464b47c3d3b98dd0499f03a37
Andrew <a.macmurray@icloud.com>
2016-04-09 10:44:05 +0100

    remove notes.js in backend

--- backend/lib/routes/notes.js
+++
@@ -1,9 +0,0 @@
-export default {
-  path: '/feedback/{username}',
-  method: 'post',
-  handler: (request, reply) => {
-    console.log(request.params.username)
-    console.log(request.payload)
-    reply(request.payload)
-  }
-}

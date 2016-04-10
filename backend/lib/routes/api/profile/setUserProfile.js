304f879151e8ac0e9410424fdd168f7a342fe8b2
Elias <elias0789@gmail.com>
2016-04-10 01:03:50 +0100

    Add logging

--- backend/lib/routes/api/profile/setUserProfile.js
+++ backend/lib/routes/api/profile/setUserProfile.js
@@ -5,8 +5,10 @@ export default (client, userType, username, payload, reply) => {
   getUserProfile(client, userType, username)
     .then((result) => {
       if (result) {
+        console.log(userType + ' user ' + username + ' already exists')
         reply({success: true, data: result})
       } else {
+        console.log(userType + ' user ' + username + ' doesn\'t exists')
         ipcAddUser(username, payload.firstName + payload.lastName)
           .then((_result) => {
             payload.apiId = _result.newUserData.uname
@@ -14,6 +16,7 @@ export default (client, userType, username, payload, reply) => {
       return setUserProfile(client, userType, username, payload)
     })
           .then((_result) => {
+            console.log(userType + ' user ' + username + ' added to DB with result: ' + _result)
       reply({success: true, data: payload})
     })
     .catch((err) => {
